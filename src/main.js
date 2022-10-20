ff8e5684a35d367fa2ddfbd069ce264155fd1eb5
arilthon <arilthony@gmail.com>
2022-10-20 20:18:05 -0300

    aula-03 finalizada

--- src/main.js
+++ src/main.js
@@ -48,12 +48,12 @@ const cardNumberPattern = {
   mask: [
     {
       mask: "0000 0000 0000 0000",
-      regex: /4\d{0.15}/,
+      regex: /^4\d{0,15}/,
       cardType: "visa",
     },
     {
       mask: "0000 0000 0000 0000",
-      regex: /(^5[1-5]\d{0,2}|22[2-9]\d|^2[3-7]\d{0,2})\d{0,12}/,
+      regex: /(^5[1-5]\d{0,2}|^22[2-9]\d|^2[3-7]\d{0,2})\d{0,12}/,
       cardType: "mastercard",
     },
     {
@@ -66,8 +66,55 @@ const cardNumberPattern = {
     const foundMask = dynamicMasked.compiledMasks.find(({ regex }) => {
       return number.match(regex)
     })
-    console.log(foundMask)
     return foundMask
   },
 }
 const cardNumberMasked = IMask(cardNumber, cardNumberPattern)
+
+const addButton = document.querySelector("#add-card")
+addButton.addEventListener("click", () => {
+  console.log("Cartão foi adicionado.")
+})
+
+document.querySelector("form").addEventListener("submit", (event) => {
+  event.preventDefault()
+})
+
+const cardHolder = document.querySelector("#card-holder")
+cardHolder.addEventListener("input", (e) => {
+  const ccHolder = document.querySelector(".cc-holder .value")
+  ccHolder.textContent = cardHolder.value || "FULANO DA SILVA" 
+})
+
+securityCodeMasked.on("accept", () => {
+updateSecurityCode(securityCodeMasked.value)
+})
+
+function updateSecurityCode(code){
+  const ccSecurity = document.querySelector(".cc-security .value")
+
+  ccSecurity.textContent = code || "123"
+
+}
+
+cardNumberMasked.on("accept", () => {
+  const cardType = cardNumberMasked.masked.currentMask.cardType
+  console.log(cardType)
+  setCardType(cardType )
+  updateCardNumber(cardNumberMasked.value)
+})
+
+function updateCardNumber(number){
+const ccNumber = document.querySelector(".cc-number")
+ccNumber.textContent = number || "1234 5678 9012 3456"
+}
+
+expirationDateMasked.on("accept", () => {
+  updateExpirationDate(expirationDateMasked.value)
+})
+
+function updateExpirationDate(date){
+  const ccExpirationDate = document.querySelector(".cc-extra .value")
+
+  ccExpirationDate.textContent = date || "02/32"
+}
